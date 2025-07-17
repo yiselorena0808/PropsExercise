ede59e3b76b8032833ef11d01181cac5aa6e01fb
Lorena <yiselorenasalzargmail.com>
2025-07-17 17:14:03 -0500

    cambios

--- contextPropms/src/VerProps.tsx
+++ contextPropms/src/VerProps.tsx
@@ -4,8 +4,8 @@ import Detalles from "./DetallesProps";
 const VerProps:React.FC<{nombreRol:string,descripcion:string}>=({nombreRol,descripcion})=>{
     return(
         <div>
-            <p>Aqui puedes ver tu rol {nombreRol}</p>
-            <p>Aqui puedes ver tu descripcion{descripcion}</p>
+            <p>Aqui puedes ver tu rol: {nombreRol}</p>
+            <p>Aqui puedes ver tu descripcion: {descripcion}</p>
         </div>
     )
 }
